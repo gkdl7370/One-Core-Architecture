2a8353938583bfc34437ab5d79bbc5f7b4778766
JiHo <gkdl7370@gmail.com>
2026-04-06 20:39:55 +0900

    feat: Initial Spring Boot project setup

---
+++ src/main/java/com/onecore/one_core_architecture/OneCoreArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.onecore.one_core_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCoreArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCoreArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onecore/one_core_architecture/OneCoreArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onecore.one_core_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCoreArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
